68fd509387184278bf67d33594d5e1eb17b807db
PAVANKUMAR KUNTOLLA <kuntollapavankumar@gmail.com>
2024-10-12 12:48:01 +0530

    added hello api

--- app/main.py
+++ app/main.py
@@ -22,3 +22,8 @@ async def startup_event():
     # Include the router from ItemAPI
     app.include_router(item_api.get_router())  # Call get_router() to include it
     app.include_router(clock_in_api.get_router())
+
+# Sample /hello GET endpoint
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello, Welcome to Vodex AI!"}
